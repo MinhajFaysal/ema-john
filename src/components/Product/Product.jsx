f02d0b6444b7bb133208fc3604ae330c2d289f52
MinhajFaysal <minhajfaysal33@gmail.com>
2023-08-05 18:13:03 +0600

    Product cart added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,8 +4,11 @@ import './Product.css'
 const Product = (props) => {
     const { img, name, price, manufacturer } = props.product;
     return (
-        <div>
+        <div className='product'>
             <img src={img} alt="" />
+            <h3>{name}</h3>
+            <h4>Price: {price}$</h4>
+            <p>Manufacturer: {manufacturer}</p>
         </div>
     );
 };
