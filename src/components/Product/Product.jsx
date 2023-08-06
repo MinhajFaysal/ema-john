06164e92b36c1852d4561df55557262586a38327
MinhajFaysal <minhajfaysal33@gmail.com>
2023-08-06 13:14:51 +0600

    handler added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props);
     const { img, name, price, seller, ratings } = props.product   // props?.product || {};
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +14,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
-import Summary from '../Order Summary/Summary';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -9,7 +8,12 @@ const Shop = () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log('product added', product);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -17,11 +21,19 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <Summary></Summary>
+                <h2>Order Summary</h2>
+                <p>Selected Items:</p>
+                <p>Total Price: $</p>
+                <p>Total Shipping Charge: $</p>
+                <p>Tax: $</p>
+                <h2>Grand Total: $</h2>
+                <a href="">Clear Cart</a> <br />
+                <a href="">Order Review</a>
             </div>
         </div>
     );
