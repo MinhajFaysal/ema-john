1ac07a20d3fcb461387de0ee5b7968e736879e65
MinhajFaysal <minhajfaysal33@gmail.com>
2023-08-07 13:19:39 +0600

    cat calculation done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,19 +5,24 @@ const Cart = (props) => {
     // const cart = props.cart;  // option 1
     const { cart } = props;    // option 2
 
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for (const product of cart) {
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+
+    const totalTax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + totalTax;
 
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total}</p>
-            <p>Total Shipping Charge: $</p>
-            <p>Tax: $</p>
-            <h2>Grand Total: $</h2>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${totalTax.toFixed(2)}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
             <a href="">Clear Cart</a> <br />
             <a href="">Order Review</a>
         </div>
